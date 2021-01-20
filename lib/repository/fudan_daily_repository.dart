5b1133f1a8430fa5d1baa4803dbcd4b79ef3775f
w568w <1278297578@qq.com>
2021-01-20 11:48:14 +0800

    Resupport Crowdedness query

--- lib/repository/fudan_daily_repository.dart
+++ lib/repository/fudan_daily_repository.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
-
 import 'package:dan_xi/model/person.dart';
+import 'package:dan_xi/public_extension_methods.dart';
 import 'package:dan_xi/repository/uis_login_tool.dart';
+import 'package:dan_xi/util/retryer.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:intl/intl.dart';
-import 'package:dan_xi/public_extension_methods.dart';
 
 import 'inpersistent_cookie_manager.dart';
 
@@ -29,22 +28,20 @@ class FudanDailyRepository {
 
   factory FudanDailyRepository.getInstance() => _instance;
 
-  Future<dynamic> _getHistoryInfo(PersonInfo info, {int retryTimes = 5}) async {
+  Future<dynamic> _getHistoryInfo(PersonInfo info) async {
     _info = info;
-    for (int i = 0; i < retryTimes; i++) {
-      await UISLoginTool.loginUIS(_dio, LOGIN_URL, _cookieJar, _info);
-      var res = await _dio.get(GET_INFO_URL);
-      try {
-        return res.data['d'];
-      } catch (e) {
-        print(e);
-      }
+    await UISLoginTool.loginUIS(_dio, LOGIN_URL, _cookieJar, _info);
+    var res = await _dio.get(GET_INFO_URL);
+    try {
+      return res.data['d'];
+    } catch (e) {
+      print(e);
     }
     return null;
   }
 
   Future<bool> hasTick(PersonInfo info) async {
-    _historyData = await _getHistoryInfo(info);
+    _historyData = await Retryer.runAsyncWithRetry(() => _getHistoryInfo(info));
     print("Last Tick Date:${_historyData['info']['date']}");
     return _historyData['info']['date'] ==
         new DateFormat('yyyyMMdd').format(DateTime.now());
@@ -52,7 +49,8 @@ class FudanDailyRepository {
 
   Future<void> tick(PersonInfo info) async {
     if (_historyData == null) {
-      _historyData = await _getHistoryInfo(info);
+      _historyData =
+          await Retryer.runAsyncWithRetry(() => _getHistoryInfo(info));
     }
     var headers = {
       "User-Agent":
