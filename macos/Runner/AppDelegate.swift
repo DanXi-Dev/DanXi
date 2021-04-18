cd47b3f885506b9f8c5b0b999fe98a03634c00de
singularity <zhaoxingjian@gmail.com>
2021-04-18 12:12:05 +0800

    Add macOS icon

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,11 @@
 import Cocoa
+import Firebase
 import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    FirebaseApp.configure()
     return true
   }
 }
