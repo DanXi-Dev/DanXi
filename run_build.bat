3c224fb91d683422e54f7810d578282e5a6efc46
w568w <1278297578@qq.com>
2021-05-30 17:03:11 +0800

    Add a build batch script for Windows build

---
+++ run_build.bat
@@ -0,0 +1,35 @@
+@echo off
+echo.
+echo Warning: Before building task, ensure that you have uncommented
+echo the line "signingConfig signingConfigs.release" in android/app/build.gradle,
+echo and choose your signing key in android/key.properties.
+echo.
+set /P version_code=Input your version name to continue:
+echo Start building...
+
+:build_android
+echo Build for Android...
+echo.
+flutter build android
+move build\app\outputs\flutter-apk\app-release.apk build\app\DanXi-%version_code%-release.android.apk
+echo.
+
+:build_windows
+echo Build for Windows...
+echo.
+flutter build windows
+7z a -r -sse build\app\DanXi-%version_code%-release.windows-x64.zip build\windows\runner\Release
+echo.
+
+:build_app_bundle
+echo Build for App Bundle (Google Play Distribution)...
+echo.
+echo Ensure that you have choose your signing key in android/key.properties.
+echo.
+echo Press any key to continue.
+pause
+flutter build appbundle
+move build\app\outputs\bundle\release\app-release.apk build\app\DanXi-%version_code%-release.android.aab
+
+:end_success
+echo Build success.
