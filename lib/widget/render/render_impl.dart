ceb41792c9e2fcefb4c3a98213ab3fd9175c05e1
w568w <1278297578@qq.com>
2021-07-13 13:57:00 +0800

    update: unify the font size of HtmlRender and MarkdownRender

--- lib/widget/render/render_impl.dart
+++ lib/widget/render/render_impl.dart
@@ -15,9 +15,11 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'package:dan_xi/util/platform_universal.dart';
 import 'package:dan_xi/util/viewport_utils.dart';
 import 'package:dan_xi/widget/auto_network_image.dart';
 import 'package:dan_xi/widget/render/base_render.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_html/flutter_html.dart';
@@ -26,13 +28,15 @@ import 'package:dan_xi/widget/image_render_x.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
+const double kFontSize = 16.0;
+
 BaseRender kHtmlRender = (BuildContext context, String content,
     LinkTapCallback onTapImage, LinkTapCallback onTapLink) {
   double imageWidth = ViewportUtils.getMainNavigatorWidth(context) * 0.75;
   Style noPaddingStyle = Style(
     margin: EdgeInsets.zero,
     padding: EdgeInsets.zero,
-    fontSize: FontSize(16),
+    fontSize: FontSize(kFontSize),
   );
   return Html(
     shrinkWrap: true,
@@ -60,11 +64,37 @@ BaseRender kHtmlRender = (BuildContext context, String content,
     onImageTap: (url, _, __, ___) => onTapImage(url),
   );
 };
+
+MarkdownStyleSheet _getMarkdownStyleSheetFromPlatform(BuildContext context) {
+  if (PlatformX.isCupertino(context)) {
+    return MarkdownStyleSheet.fromCupertinoTheme(CupertinoTheme.of(context));
+  } else {
+    return MarkdownStyleSheet.fromTheme(Theme.of(context));
+  }
+}
+
+MarkdownStyleSheet _fontSizeOverride(
+    MarkdownStyleSheet sheet, double fontSize) {
+  return sheet.copyWith(
+    p: sheet.p?.copyWith(fontSize: fontSize),
+    a: sheet.a?.copyWith(fontSize: fontSize),
+    em: sheet.em?.copyWith(fontSize: fontSize),
+    strong: sheet.strong?.copyWith(fontSize: fontSize),
+    del: sheet.del?.copyWith(fontSize: fontSize),
+    blockquote: sheet.blockquote?.copyWith(fontSize: fontSize),
+    listBullet: sheet.listBullet?.copyWith(fontSize: fontSize),
+    checkbox: sheet.checkbox?.copyWith(fontSize: fontSize),
+  );
+}
+
 BaseRender kMarkdownRender = (BuildContext context, String content,
     LinkTapCallback onTapImage, LinkTapCallback onTapLink) {
   double imageWidth = ViewportUtils.getMainNavigatorWidth(context) * 0.75;
+
   return MarkdownBody(
     data: content,
+    styleSheet: _fontSizeOverride(
+        _getMarkdownStyleSheetFromPlatform(context), kFontSize),
     onTapLink: (String text, String href, String title) =>
         onTapLink?.call(href),
     imageBuilder: (Uri uri, String title, String alt) {
