5b2f90d44ce8fa1e754635cf2fa3b40f7e27d1b0
singularity <zhaoxingjian@gmail.com>
2021-07-20 00:19:27 +0800

    fix: rebuild when switching tabs

--- lib/main.dart
+++ lib/main.dart
@@ -174,7 +174,9 @@ class DanxiApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate
           ],
           supportedLocales: S.delegate.supportedLocales,
-          home: HomePage(),
+          home: MasterDetailController(
+            masterPage: HomePage(),
+          ),
           // Configure the page route behaviour of the whole app
           onGenerateRoute: (settings) {
             final Function pageContentBuilder = DanxiApp.routes[settings.name];
@@ -636,93 +638,90 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
           ChangeNotifierProvider.value(value: _personInfo),
           Provider.value(value: _preferences),
         ],
-        child: MasterDetailController(
-          masterPage: PlatformScaffold(
-            iosContentBottomPadding:
-                _subpage[_pageIndex.value].needBottomPadding,
-            iosContentPadding: _subpage[_pageIndex.value].needPadding,
-            appBar: PlatformAppBar(
-              cupertino: (_, __) => CupertinoNavigationBarData(
-                title: MediaQuery(
-                  data: MediaQueryData(
-                      textScaleFactor: MediaQuery.textScaleFactorOf(context)),
-                  child: TopController(
-                    child: _appTitleWidgetBuilder[_pageIndex.value](context),
-                    controller: PrimaryScrollController.of(context),
-                  ),
-                ),
-              ),
-              material: (_, __) => MaterialAppBarData(
-                title: TopController(
-                  child: Text(
-                    S.of(context).app_name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
+        child: PlatformScaffold(
+          iosContentBottomPadding: _subpage[_pageIndex.value].needBottomPadding,
+          iosContentPadding: _subpage[_pageIndex.value].needPadding,
+          appBar: PlatformAppBar(
+            cupertino: (_, __) => CupertinoNavigationBarData(
+              title: MediaQuery(
+                data: MediaQueryData(
+                    textScaleFactor: MediaQuery.textScaleFactorOf(context)),
+                child: TopController(
+                  child: _appTitleWidgetBuilder[_pageIndex.value](context),
                   controller: PrimaryScrollController.of(context),
                 ),
               ),
-              leading: leadingButton,
-              trailingActions: trailingButtons,
             ),
-            body: IndexedStack(
-              index: _pageIndex.value,
-              children: _subpage,
-            ),
-
-            // 2021-5-19 @w568w:
-            // Override the builder to prevent the repeatedly built states.
-            // I don't know why it works...
-            cupertinoTabChildBuilder: (_, index) => _subpage[index],
-            bottomNavBar: PlatformNavBar(
-              items: [
-                BottomNavigationBarItem(
-                  //backgroundColor: Colors.purple,
-                  icon: PlatformX.isAndroid
-                      ? Icon(Icons.dashboard)
-                      : Icon(SFSymbols.square_stack_3d_up_fill),
-                  label: S.of(context).dashboard,
+            material: (_, __) => MaterialAppBarData(
+              title: TopController(
+                child: Text(
+                  S.of(context).app_name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-                BottomNavigationBarItem(
-                  //backgroundColor: Colors.indigo,
-                  icon: PlatformX.isAndroid
-                      ? Icon(Icons.forum)
-                      : Icon(SFSymbols.text_bubble),
-                  label: S.of(context).forum,
-                ),
-                BottomNavigationBarItem(
-                  //backgroundColor: Colors.blue,
-                  icon: PlatformX.isAndroid
-                      ? Icon(Icons.calendar_today)
-                      : Icon(SFSymbols.calendar),
-                  label: S.of(context).timetable,
-                ),
-                BottomNavigationBarItem(
-                  //backgroundColor: Theme.of(context).primaryColor,
-                  icon: PlatformX.isAndroid
-                      ? Icon(Icons.settings)
-                      : Icon(SFSymbols.gear_alt),
-                  label: S.of(context).settings,
-                ),
-              ],
-              currentIndex: _pageIndex.value,
-              material: (_, __) => MaterialNavBarData(
-                type: BottomNavigationBarType.fixed,
-                selectedIconTheme:
-                    BottomNavigationBarTheme.of(context).selectedIconTheme,
-                unselectedIconTheme:
-                    BottomNavigationBarTheme.of(context).unselectedIconTheme,
+                controller: PrimaryScrollController.of(context),
               ),
-              itemChanged: (index) {
-                if (index != _pageIndex.value) {
-                  // List<ScrollPosition> positions =
-                  //     PrimaryScrollController.of(context).positions.toList();
-                  // for (var p in positions.skip(1)) {
-                  //   PrimaryScrollController.of(context).detach(p);
-                  // }
-                  setState(() => _pageIndex.value = index);
-                }
-              },
             ),
+            leading: leadingButton,
+            trailingActions: trailingButtons,
+          ),
+          body: IndexedStack(
+            index: _pageIndex.value,
+            children: _subpage,
+          ),
+
+          // 2021-5-19 @w568w:
+          // Override the builder to prevent the repeatedly built states.
+          // I don't know why it works...
+          cupertinoTabChildBuilder: (_, index) => _subpage[index],
+          bottomNavBar: PlatformNavBar(
+            items: [
+              BottomNavigationBarItem(
+                //backgroundColor: Colors.purple,
+                icon: PlatformX.isAndroid
+                    ? Icon(Icons.dashboard)
+                    : Icon(SFSymbols.square_stack_3d_up_fill),
+                label: S.of(context).dashboard,
+              ),
+              BottomNavigationBarItem(
+                //backgroundColor: Colors.indigo,
+                icon: PlatformX.isAndroid
+                    ? Icon(Icons.forum)
+                    : Icon(SFSymbols.text_bubble),
+                label: S.of(context).forum,
+              ),
+              BottomNavigationBarItem(
+                //backgroundColor: Colors.blue,
+                icon: PlatformX.isAndroid
+                    ? Icon(Icons.calendar_today)
+                    : Icon(SFSymbols.calendar),
+                label: S.of(context).timetable,
+              ),
+              BottomNavigationBarItem(
+                //backgroundColor: Theme.of(context).primaryColor,
+                icon: PlatformX.isAndroid
+                    ? Icon(Icons.settings)
+                    : Icon(SFSymbols.gear_alt),
+                label: S.of(context).settings,
+              ),
+            ],
+            currentIndex: _pageIndex.value,
+            material: (_, __) => MaterialNavBarData(
+              type: BottomNavigationBarType.fixed,
+              selectedIconTheme:
+                  BottomNavigationBarTheme.of(context).selectedIconTheme,
+              unselectedIconTheme:
+                  BottomNavigationBarTheme.of(context).unselectedIconTheme,
+            ),
+            itemChanged: (index) {
+              if (index != _pageIndex.value) {
+                // List<ScrollPosition> positions =
+                //     PrimaryScrollController.of(context).positions.toList();
+                // for (var p in positions.skip(1)) {
+                //   PrimaryScrollController.of(context).detach(p);
+                // }
+                setState(() => _pageIndex.value = index);
+              }
+            },
           ),
         ),
       );
