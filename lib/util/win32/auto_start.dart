DanXi-Dev/DanXi

commit 1f33fb3fdf7ee809e088146383e848b5b0a43bb1

misc: apply automated dart fix

lib/util/win32/auto_start.dart
@@ -30,7 +30,7 @@ class WindowsAutoStart {
     String? path;
     try {
       path = Registry.getStringKey(hKey, KEY_NAME);
-    } catch (ignored) {} finally {
+    } finally {
       RegCloseKey(hKey);
     }
     return PlatformX.executablePath == path;

lib/page/subpage_dashboard.dart
@@ -221,10 +221,10 @@ class _HomeSubpageState extends State<HomeSubpage>
           onDismissed: () => _notifications.remove(e),
         )));
     List<Widget> _currentCardChildren = [];
-    widgetSequence.forEach((element) {
-      if (!element.enabled!) return;
+    for (var element in widgetSequence) {
+      if (!element.enabled!) continue;
       if (element.internalString == 'new_card') {
-        if (_currentCardChildren.isEmpty) return;
+        if (_currentCardChildren.isEmpty) continue;
         _widgets.add(Card(
           child: Column(
             children: _currentCardChildren,
@@ -243,12 +243,12 @@ class _HomeSubpageState extends State<HomeSubpage>
               widgetMap[element.internalString!] as FeatureContainer;
           if (!checkFeature(
               container.childFeature, StateProvider.personInfo.value!.group)) {
-            return;
+            continue;
           }
         }
         _currentCardChildren.add(widgetMap[element.internalString!]!);
       }
-    });
+    }
     if (_currentCardChildren.isNotEmpty) {
       _widgets.add(Card(
         child: Column(

lib/repository/fdu/data_center_repository.dart
@@ -78,7 +78,7 @@ class DataCenterRepository extends BaseRepositoryWithDio {
       });
 
   Future<Map<String, TrafficInfo>> _getCrowdednessInfo(int areaCode) async {
-    var result = Map<String, TrafficInfo>();
+    var result = <String, TrafficInfo>{};
     var response = await dio!.get(DINING_DETAIL_URL);
 
     //If it's not time for a meal
@@ -87,19 +87,19 @@ class DataCenterRepository extends BaseRepositoryWithDio {
     }
     var dataString =
         response.data.toString().between("}", "</script>", headGreedy: false)!;
-    var jsonExtraction = new RegExp(r'\[.+\]').allMatches(dataString);
+    var jsonExtraction = RegExp(r'\[.+\]').allMatches(dataString);
     List names = jsonDecode(jsonExtraction
         .elementAt(areaCode * 3)
         .group(0)!
-        .replaceAll("\'", "\""));
+        .replaceAll("'", "\""));
     List? cur = jsonDecode(jsonExtraction
         .elementAt(areaCode * 3 + 1)
         .group(0)!
-        .replaceAll("\'", "\""));
+        .replaceAll("'", "\""));
     List? max = jsonDecode(jsonExtraction
         .elementAt(areaCode * 3 + 2)
         .group(0)!
-        .replaceAll("\'", "\""));
+        .replaceAll("'", "\""));
     for (int i = 0; i < names.length; i++) {
       result[names[i]] = TrafficInfo(int.parse(cur![i]), int.parse(max![i]));
     }

lib/repository/fdu/empty_classroom_repository.dart
@@ -59,9 +59,9 @@ class EmptyClassroomRepository extends BaseRepositoryWithDio {
         ? response.data
         : jsonDecode(response.data.toString());
     final Map buildingInfo = json['d']['list'];
-    buildingInfo.values.forEach((element) {
+    for (var element in buildingInfo.values) {
       if (element is List) {
-        element.forEach((element) {
+        for (var element in element) {
           RoomInfo info = RoomInfo(element['name'], date, element['roomrl']);
           info.busy = [];
           if (element['kxsds'] is Map) {
@@ -70,9 +70,9 @@ class EmptyClassroomRepository extends BaseRepositoryWithDio {
                 .forEach((element) => info.busy!.add(element != "闲"));
             result.add(info);
           }
-        });
+        }
       }
-    });
+    }
     return result;
   }
 

lib/util/bmob/bmob/bmob_batch.dart
@@ -22,7 +22,7 @@ class BmobBatch {
 
   Future<List> process(String method, List<BmobObject> bmobObjects) async {
     List list = [];
-    Map params = Map();
+    Map params = {};
 
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? userJson = prefs.get("user") as String?;
@@ -36,7 +36,7 @@ class BmobBatch {
         //过滤BmobUser类型的处理，因为批处理操作不支持对User表的操作
         debugPrint("BmobUser does not support batch operations");
       } else {
-        Map single = Map();
+        Map single = {};
         single["method"] = method;
         if (method == "PUT" || method == "DELETE") {
           //批量更新和批量删除

lib/util/bmob/bmob/bmob_dio.dart
@@ -29,7 +29,7 @@ class BmobDio {
 
   ///无参构造方法
   BmobDio() {
-    options = new BaseOptions(
+    options = BaseOptions(
       //基地址
       baseUrl: Bmob.bmobHost,
       //连接服务器的超时时间，单位是毫秒。
@@ -42,7 +42,7 @@ class BmobDio {
 //      },
     );
 
-    dio = new Dio(options);
+    dio = Dio(options);
   }
 
   ///获取16位随机字符串
@@ -72,22 +72,20 @@ class BmobDio {
 
   ///md5编码
   String generateMd5(String origin) {
-    var content = new Utf8Encoder().convert(origin);
+    var content = const Utf8Encoder().convert(origin);
     var digest = md5.convert(content);
     return hex.encode(digest.bytes);
   }
 
   ///获取时间戳 秒
   getSafeTimestamp() {
-    int second = (new DateTime.now().millisecondsSinceEpoch) ~/ 1000;
+    int second = (DateTime.now().millisecondsSinceEpoch) ~/ 1000;
     return second.toString();
   }
 
   ///单例模式
   static BmobDio? getInstance() {
-    if (instance == null) {
-      instance = BmobDio();
-    }
+    instance ??= BmobDio();
     return instance;
   }
 
@@ -171,7 +169,7 @@ class BmobDio {
 
   ///获取请求头
   getHeaders(path, data) {
-    Map<String, dynamic> map = Map();
+    Map<String, dynamic> map = {};
 
     if (Bmob.bmobAppId.isNotEmpty) {
       //没有加密

lib/util/bmob/bmob/realtime/client.dart
@@ -198,9 +198,7 @@ class Client {
             arguments[1] = ackParts[1];
           }
           String data = "";
-          if (arguments != null) {
-            data += "+" + arguments.toString();
-          }
+          data += "+" + arguments.toString();
           String part = parts[1];
           String ack = "6::$part$data";
           webSocket!.add(ack);

lib/util/platform_universal.dart
@@ -53,7 +53,7 @@ class PlatformX {
       deviceId = await PlatformDeviceId.getDeviceId;
     } catch (ignored) {}
     if (deviceId == null) {
-      return Uuid().v4();
+      return const Uuid().v4();
     } else {
       return deviceId;
     }

commit 5d7e2db80ded8f7c7161ada14431addc9a9ed22e

fix: broken settings on Windows in commit 1f33fb3fdf7ee809e088146383e848b5b0a43bb1

lib/util/win32/auto_start.dart
@@ -30,6 +30,7 @@ class WindowsAutoStart {
     String? path;
     try {
       path = Registry.getStringKey(hKey, KEY_NAME);
+    } catch (_) {
     } finally {
       RegCloseKey(hKey);
     }

lib/util/win32/registry.dart
@@ -62,7 +62,7 @@ class Registry {
         case ERROR_SUCCESS:
           return pvData.cast<Utf16>().toDartString();
         default:
-          throw Exception('unknown error');
+          throw Exception('unknown error $status');
       }
     } finally {
       free(pvData);