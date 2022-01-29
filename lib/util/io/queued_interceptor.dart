8eebdea55345f9fd79a720084106823fa87a70e5
w568w <1278297578@qq.com>
2022-01-29 12:11:04 +0800

    feat: add a queued_interceptor to limit the amount of request concurrency

---
+++ lib/util/io/queued_interceptor.dart
@@ -0,0 +1,88 @@
+/*
+ *     Copyright (C) 2022  DanXi-Dev
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'dart:async';
+
+import 'package:dan_xi/model/pair.dart';
+import 'package:dio/dio.dart';
+
+/// The number of how many requests we allow to be executed simultaneously.
+const _kQueueLengthLimit = 4;
+
+/// A [QueuedInterceptor] can limit the number of concurrent requests.
+class LimitedQueuedInterceptor extends QueuedInterceptor {
+  static final LimitedQueuedInterceptor _instance =
+      LimitedQueuedInterceptor._();
+
+  /// The completer of requests executing at the moment.
+  final List<Completer<void>> _requestWorkingQueue = [];
+
+  /// The requests waiting to be executed.
+  final List<Pair<RequestOptions, RequestInterceptorHandler>>
+      _requestWaitingQueue = [];
+
+  LimitedQueuedInterceptor._();
+
+  factory LimitedQueuedInterceptor.getInstance() => _instance;
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    // Notify a completer in queue to complete itself.
+    //
+    // Note: We do NOT care the matches between [RequestOptions] and [Completer<void>],
+    // the queue is only used to indicate how many requests are being executed now.
+    _requestWorkingQueue.removeAt(0).complete();
+    print(
+        "-> New error, working queue length = ${_requestWorkingQueue.length}");
+    handler.next(err);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    // Notify a completer in queue to complete itself.
+    _requestWorkingQueue.removeAt(0).complete();
+    print(
+        "-> New response, working queue length = ${_requestWorkingQueue.length}");
+    handler.next(response);
+  }
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    // If there are fewer requests than [_kQueueLengthLimit], we just request at once.
+    if (_requestWorkingQueue.length < _kQueueLengthLimit) {
+      _requestWorkingQueue.add(Completer());
+      print(
+          "<- New request directly goes, working queue length = ${_requestWorkingQueue.length}");
+      handler.next(options);
+      return;
+    }
+    // Else, we add it to the [_requestWaitingQueue] and wait for any request to complete.
+    print(
+        "!! New request has to wait now, because working queue length = ${_requestWorkingQueue.length}");
+    _requestWaitingQueue.add(Pair(options, handler));
+
+    Future.any(_requestWorkingQueue.map((e) => e.future)).then((_) {
+      // Launch the request when we have free rocket pod.
+      Pair<RequestOptions, RequestInterceptorHandler> requestHandlerPair =
+          _requestWaitingQueue.removeAt(0);
+      _requestWorkingQueue.add(Completer());
+      print(
+          "<! New request finally gets its turn, working queue length = ${_requestWorkingQueue.length}");
+      requestHandlerPair.second.next(requestHandlerPair.first);
+    });
+  }
+}
