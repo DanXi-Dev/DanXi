6b0ba0a941aa6d220c972bfb97836d98c686b1b6
runner <runner@Mac-1649258179055.local>
2022-04-06 15:22:45 +0000

    [GitHub Action] Bump Build Number

--- lib/model/extra.g.dart
+++ lib/model/extra.g.dart
@@ -14,8 +14,7 @@ Extra _$ExtraFromJson(Map<String, dynamic> json) => Extra(
       (json['stopWords'] as List<dynamic>?)?.map((e) => e as String?).toList(),
     );
 
-Map<String, dynamic> _$ExtraToJson(Extra instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$ExtraToJson(Extra instance) => <String, dynamic>{
       'timetable': instance.timetable,
       'userAgent': instance.userAgent,
       'stopWords': instance.stopWords,
