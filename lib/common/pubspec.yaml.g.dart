1b03d90613070020895581e397a16a4296e7ccc5
runner <runner@Mac-1656084513648.local>
2022-06-24 15:40:44 +0000

    [GitHub Action] Bump Build Number

--- lib/common/pubspec.yaml.g.dart
+++ lib/common/pubspec.yaml.g.dart
@@ -40,7 +40,7 @@
 // ignore_for_file: prefer_single_quotes
 
 /// Current app version
-const String version = r'1.3.11+181';
+const String version = r'1.3.11+182';
 
 /// The major version number: "1" in "1.2.3".
 const int major = 1;
@@ -55,10 +55,10 @@ const int patch = 11;
 const List<String> pre = <String>[];
 
 /// The build identifier: "foo" in "1.2.3+foo".
-const List<String> build = <String>[r'181'];
+const List<String> build = <String>[r'182'];
 
 /// Build date in Unix Time (in seconds)
-const int timestamp = 1656046635;
+const int timestamp = 1656085234;
 
 /// Name [name]
 const String name = r'dan_xi';
