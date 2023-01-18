612e9755ccf47df1f6a9669cc770d1d0064e38c3
w568w <1278297578@qq.com>
2023-01-18 11:17:48 +0800

    fix: wrong font is selected on Windows
    See https://github.com/flutter/flutter/issues/103811 for more details.

--- lib/provider/language_manager.dart
+++ lib/provider/language_manager.dart
@@ -1,31 +1,31 @@
 import 'dart:ui';
 
+import 'package:dan_xi/common/constant.dart';
 import 'package:dan_xi/generated/l10n.dart';
 import 'package:dan_xi/provider/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import '/common/constant.dart';
-
-class LanguageManager{
-
+class LanguageManager {
   LanguageManager(this.language);
 
   final Language language;
 
-  ///Convert the [Language] to languagecode for [Locale]
-  String languageCodeGenerator(Language language) {
-    if (language == Language.SIMPLE_CHINESE) return "zh";
-    if (language == Language.ENGLISH) return "en";
-    if (language == Language.JAPANESE) return "ja";
-    return "?";
+  /// Convert the [Language] to language code for [Locale].
+  Locale languageCodeGenerator(Language language) {
+    if (language == Language.SIMPLE_CHINESE) return const Locale("zh", "CN");
+    if (language == Language.ENGLISH) return const Locale("en");
+    if (language == Language.JAPANESE) return const Locale("ja");
+    return const Locale("en");
   }
 
-  void setLanguage(){
-    S.delegate.load(Locale(languageCodeGenerator(SettingsProvider.getInstance().language)));
-    GlobalMaterialLocalizations.delegate.load(Locale(languageCodeGenerator(SettingsProvider.getInstance().language)));
-    GlobalWidgetsLocalizations.delegate.load(Locale(languageCodeGenerator(SettingsProvider.getInstance().language)));
-    GlobalCupertinoLocalizations.delegate.load(Locale(languageCodeGenerator(SettingsProvider.getInstance().language)));
-  }
+  void setLanguage() {
+    final locale =
+        languageCodeGenerator(SettingsProvider.getInstance().language);
 
+    S.delegate.load(locale);
+    GlobalMaterialLocalizations.delegate.load(locale);
+    GlobalWidgetsLocalizations.delegate.load(locale);
+    GlobalCupertinoLocalizations.delegate.load(locale);
+  }
 }
