986065f54b2f8fd06ee7f90dcad39075140ccf5b
w568w <1278297578@qq.com>
2023-01-26 06:14:00 +0000

    Deploying to gh-pages from @ DanXi-Dev/DanXi@e8688cf821b3d8ac46a5ebdb9a721c379e4d00a8 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5ac5a6d3bb3bd3acf035f1ab1043e45e",
-"index.html": "055ef6f94d4e4545477a2ec2a3abeefb",
-"/": "055ef6f94d4e4545477a2ec2a3abeefb",
-"main.dart.js": "85efaf1c515cd93bad19d4cb342acc19",
+"index.html": "cf031a1bb3070aa912969cb223612b33",
+"/": "cf031a1bb3070aa912969cb223612b33",
+"main.dart.js": "0a5af9cc1ebba55de86a9a990c9799ed",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.webp": "5d8a19c576a1ba2a24af3b99f340237d",
 "manifest.json": "012d91b9edf492757ddf24dd1b15d03e",
