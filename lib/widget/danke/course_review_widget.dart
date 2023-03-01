21d228e22ff010944d4897d9f21947ba6dd1a033
ivanfei <ivanfee@outlook.com>
2023-03-02 00:06:04 +0800

    update: review voting widget

---
+++ lib/widget/danke/course_review_widget.dart
@@ -0,0 +1,34 @@
+/*
+ *     Copyright (C) 2021  DanXi-Dev
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'dart:ui';
+
+import 'package:dan_xi/common/constant.dart';
+import 'package:dan_xi/generated/l10n.dart';
+import 'package:dan_xi/model/time_table.dart';
+import 'package:dan_xi/page/platform_subpage.dart';
+import 'package:dan_xi/provider/settings_provider.dart';
+import 'package:dan_xi/util/noticing.dart';
+import 'package:dan_xi/util/platform_universal.dart';
+import 'package:dan_xi/util/public_extension_methods.dart';
+import 'package:dan_xi/util/stream_listener.dart';
+import 'package:dan_xi/widget/danke/course_widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:provider/provider.dart';
