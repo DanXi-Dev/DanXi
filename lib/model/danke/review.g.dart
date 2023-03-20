46cb243faa2adc956879031aec8e26053b23d16c
ivanfei <ivanfee@outlook.com>
2023-03-20 14:33:02 +0800

    change: change [review]'s data structure

--- lib/model/danke/review.g.dart
+++ lib/model/danke/review.g.dart
@@ -8,18 +8,32 @@ part of 'review.dart';
 
 Review _$ReviewFromJson(Map<String, dynamic> json) => Review(
       json['id'] as int?,
+      json['reviewerId'] as int?,
       json['title'] as String?,
       json['content'] as String?,
-      json['time_created'] as String?,
-      json['rank'] as String?,
+      json['timeCreated'] as String?,
+      json['timeUpdated'] as String?,
+      json['rank'] == null
+          ? null
+          : Rank.fromJson(json['rank'] as Map<String, dynamic>),
       json['remark'] as int?,
+      json['vote'] as int?,
+      json['isMe'] as bool?,
+      json['extra'] == null
+          ? null
+          : ReviewExtra.fromJson(json['extra'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$ReviewToJson(Review instance) => <String, dynamic>{
       'id': instance.id,
+      'reviewerId': instance.reviewerId,
       'title': instance.title,
       'content': instance.content,
-      'time_created': instance.time_created,
+      'timeCreated': instance.timeCreated,
+      'timeUpdated': instance.timeUpdated,
       'rank': instance.rank,
       'remark': instance.remark,
+      'vote': instance.vote,
+      'isMe': instance.isMe,
+      'extra': instance.extra,
     };
