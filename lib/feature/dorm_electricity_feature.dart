80f0e826299de9bfcef7f65e36feb4bca9052480
KYLN24 <1296845690@qq.com>
2023-07-22 20:05:11 +0800

    doc: add many comments in dorm_electricity_feature.dart

--- lib/feature/dorm_electricity_feature.dart
+++ lib/feature/dorm_electricity_feature.dart
@@ -26,38 +26,73 @@ import 'package:dan_xi/widget/feature_item/feature_progress_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/// Every feature should extends [Feature]. You may find
+/// `lib/feature/base_feature.dart` helpful.
+///
+/// Generally to implement a feature, you should do the following things:
+/// - Set bool [clickable] to true if needed. Most features are clickable. Override
+///   [onTap] if you want to do something when user taps the feature.
+/// - Set bool [loadOnTap] to false if you want to load data immediately.
+///   If you do so, [buildFeature] will not be called until user first tap the
+///   feature.
+/// - Set [icon] to an [Icon] if needed. Select one from [Icons] or [CupertinoIcons]
+///   as you like.
+/// - Set [mainTitle] to a [String] if needed. The main title of a feature is
+///   usually the name of the feature, which remains unchanged.
+/// - Set [subTitle] to a [String] if needed. You can display information here.
+/// - Override [buildFeature].
+/// - Refer to `lib/feature/base_feature.dart` for more details.
+///
+/// If the feature relates to a network request, you should do the following things:
+/// - Create a repository in `lib/repository/` if needed. See `lib/repository/base_repository.dart`.
+///   You may find the extensive comments in `lib/repository/fdu/dorm_repository.dart`
+///   helpful.
+/// - You can initialize a [ConnectionStatus] variable to [ConnectionStatus.NONE]
+///   to indicate the connection status. Call [notifyUpdate] when the request is done.
 class DormElectricityFeature extends Feature {
+  /// The data instance. See `lib/repository/fdu/dorm_repository.dart`.
   ElectricityItem? _electricity;
 
   /// Status of the request.
+  /// Whenever the status is changed, call [notifyUpdate] to update the widget.
   ConnectionStatus _status = ConnectionStatus.NONE;
 
   Future<void> _loadData() async {
     _status = ConnectionStatus.CONNECTING;
     try {
+      // Await the repository to load data.
       _electricity = await FudanDormRepository.getInstance()
           .loadElectricityInfo(StateProvider.personInfo.value);
       _status = ConnectionStatus.DONE;
     } catch (e) {
       _status = ConnectionStatus.FAILED;
     }
+    // Remember to call [notifyUpdate] to update the widget.
     notifyUpdate();
   }
 
+  /// Load data when the feature is created.
+  ///
+  /// Only load data once.
+  /// If user needs to refresh the data, [refreshSelf] will be called on the whole
+  /// page, not just on [FeatureContainer]. The feature will be recreated then.
+  ///
+  /// If the feature is [loadOnTap], [buildFeature] will be called when user taps.
+  /// Otherwise, [buildFeature] will be called when the feature is created.
   @override
   void buildFeature([Map<String, dynamic>? arguments]) {
-    // Only load data once.
-    // If user needs to refresh the data, [refreshSelf()] will be called on the whole page,
-    // not just FeatureContainer. So the feature will be recreated then.
     if (_status == ConnectionStatus.NONE) {
       _electricity = null;
       _loadData();
     }
   }
 
+  /// the Main title of the feature, usually to be the name of the feature. We
+  /// use [S] to support i18n.
   @override
   String get mainTitle => S.of(context!).dorm_electricity;
 
+  /// The subtitle of the feature. We usually display the data here.
   @override
   String get subTitle {
     switch (_status) {
@@ -73,6 +108,9 @@ class DormElectricityFeature extends Feature {
     }
   }
 
+  /// The tertiary title of the feature. We usually display the loading progress
+  /// here. Some features have a button here to navigate to a new page or do
+  /// something else. See [WelcomeFeature] and [NextCourseFeature].
   @override
   Widget? get trailing {
     if (_status == ConnectionStatus.CONNECTING) {
@@ -91,19 +129,19 @@ class DormElectricityFeature extends Feature {
     notifyUpdate();
   }
 
+  /// Be careful that [onTap] will not be called if the feature does not have
+  /// it's first tap when [loadOnTap] is true.
+  ///
+  /// If you want to do something when user taps the feature, override this method.
+  /// A typical example is to show a modal dialog, navigate to a new page via
+  /// [smartNavigatorPush] or reload the data when it failed.
   @override
   void onTap() {
     if (_status == ConnectionStatus.DONE) {
-      final String body = '${_electricity?.dormName}\n${S
-          .of(context!)
-          .dorm_electricity_subtitle(
-          _electricity!.available, _electricity!.used)}\n\n${S
-          .of(context!)
-          .last_updated(_electricity!.updateTime.toString())}';
+      final String body =
+          '${_electricity?.dormName}\n${S.of(context!).dorm_electricity_subtitle(_electricity!.available, _electricity!.used)}\n\n${S.of(context!).last_updated(_electricity!.updateTime.toString())}';
       Noticing.showModalNotice(context!,
-          message: body, title: S
-              .of(context!)
-              .dorm_electricity);
+          message: body, title: S.of(context!).dorm_electricity);
     } else {
       refreshData();
     }
