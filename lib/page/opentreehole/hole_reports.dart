9444bfcbb66aadadc28e397d7fb77bf02f02d3a3
Ivanfei <ivanfei2218@icloud.com>
2023-11-11 22:55:16 +0800

    add: tab for audit review (undone)

--- lib/page/opentreehole/hole_reports.dart
+++ lib/page/opentreehole/hole_reports.dart
@@ -36,6 +36,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_progress_dialog/flutter_progress_dialog.dart';
+import 'package:lazy_load_indexed_stack/lazy_load_indexed_stack.dart';
 
 /// A list page showing the reports for administrators.
 class BBSReportDetail extends StatefulWidget {
@@ -51,6 +52,8 @@ class BBSReportDetailState extends State<BBSReportDetail> {
   final PagedListViewController<OTReport> _listViewController =
       PagedListViewController();
 
+  int _tabIndex = 0;
+
   /// Reload/load the (new) content and set the [_content] future.
   Future<List<OTReport>?> _loadContent(int page) =>
       OpenTreeHoleRepository.getInstance().adminGetReports(page * 10, 10);
@@ -58,54 +61,85 @@ class BBSReportDetailState extends State<BBSReportDetail> {
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
-      material: (_, __) =>
-          MaterialScaffoldData(resizeToAvoidBottomInset: false),
-      cupertino: (_, __) =>
-          CupertinoPageScaffoldData(resizeToAvoidBottomInset: false),
-      iosContentPadding: false,
-      iosContentBottomPadding: false,
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      appBar: PlatformAppBarX(
-          title: TopController(
-        controller: PrimaryScrollController.of(context),
-        child: Text(S.of(context).reports),
-      )),
-      body: StatefulBuilder(
-        // The builder widget updates context so that MediaQuery below can use the correct context (that is, Scaffold considered)
-        builder: (context, setState) {
-          return RefreshIndicator(
-            edgeOffset: MediaQuery.of(context).padding.top,
-            color: Theme.of(context).colorScheme.secondary,
-            backgroundColor: Theme.of(context).dialogBackgroundColor,
-            onRefresh: () async {
-              HapticFeedback.mediumImpact();
-              await refreshSelf();
-              await _listViewController.notifyUpdate(
-                  useInitialData: false, queueDataClear: false);
-            },
-            child: PagedListView<OTReport>(
-              startPage: 0,
-              pagedController: _listViewController,
-              withScrollbar: true,
-              scrollController: PrimaryScrollController.of(context),
-              dataReceiver: _loadContent,
-              builder: _getListItems,
-              loadingBuilder: (BuildContext context) => Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(child: PlatformCircularProgressIndicator()),
-              ),
-              endBuilder: (context) => Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 16),
-                  child: Text(S.of(context).end_reached),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
+        material: (_, __) =>
+            MaterialScaffoldData(resizeToAvoidBottomInset: false),
+        cupertino: (_, __) =>
+            CupertinoPageScaffoldData(resizeToAvoidBottomInset: false),
+        iosContentPadding: false,
+        iosContentBottomPadding: false,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: PlatformAppBarX(
+            title: TopController(
+          controller: PrimaryScrollController.of(context),
+          child: Text(S.of(context).reports),
+        )),
+        body: SafeArea(
+            bottom: false,
+            child: StatefulBuilder(
+              // The builder widget updates context so that MediaQuery below can use the correct context (that is, Scaffold considered)
+              builder: (context, setState) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: CupertinoSlidingSegmentedControl<int>(
+                        onValueChanged: (int? value) {
+                          setState(() {
+                            _tabIndex = value!;
+                          });
+                        },
+                        groupValue: _tabIndex,
+                        children: ["Report", "Audit"]
+                            .map((t) => Text(t))
+                            .toList()
+                            .asMap(),
+                        // todo reformat the code
+                      ),
+                    ),
+                    Expanded(
+                      child: LazyLoadIndexedStack(index: _tabIndex, children: [
+                        _buildReportPage(),
+                      ]),
+                    ),
+                  ],
+                );
+              },
+            )));
   }
+
+  Widget _buildReportPage() => RefreshIndicator(
+        edgeOffset: MediaQuery.of(context).padding.top,
+        color: Theme.of(context).colorScheme.secondary,
+        backgroundColor: Theme.of(context).dialogBackgroundColor,
+        onRefresh: () async {
+          HapticFeedback.mediumImpact();
+          await refreshSelf();
+          await _listViewController.notifyUpdate(
+              useInitialData: false, queueDataClear: false);
+        },
+        child: PagedListView<OTReport>(
+          startPage: 0,
+          pagedController: _listViewController,
+          withScrollbar: true,
+          scrollController: PrimaryScrollController.of(context),
+          dataReceiver: _loadContent,
+          builder: _getListItems,
+          loadingBuilder: (BuildContext context) => Container(
+            padding: const EdgeInsets.all(8),
+            child: Center(child: PlatformCircularProgressIndicator()),
+          ),
+          endBuilder: (context) => Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text(S.of(context).end_reached),
+            ),
+          ),
+        ),
+      );
+
+  // Widget _buildAuditPage() {
+  //   return
+  // }
 
   List<Widget> _buildContextMenu(
           BuildContext pageContext, BuildContext menuContext, OTReport e) =>
