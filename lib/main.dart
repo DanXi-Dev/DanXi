aa3e27600ab3bc22ed87a5844f8287196cf2e583
w568w <1278297578@qq.com>
2024-02-29 11:14:53 +0800

    feat: close #308

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,7 @@ import 'package:flutter_fgbg/flutter_fgbg.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:git_info/git_info.dart';
 import 'package:material_color_generator/material_color_generator.dart';
 import 'package:provider/provider.dart';
 import 'package:xiao_mi_push_plugin/xiao_mi_push_plugin.dart';
@@ -79,8 +80,8 @@ void main() {
 
   // Init Mi push Service.
   if (PlatformX.isAndroid) {
-    XiaoMiPushPlugin.init(
-        appId: "2882303761519940685", appKey: "5821994071685");
+    unawaited(XiaoMiPushPlugin.init(
+        appId: "2882303761519940685", appKey: "5821994071685"));
   }
 
   // Init Feature registration.
@@ -324,7 +325,8 @@ class DanxiApp extends StatelessWidget {
       child: MultiProvider(providers: [
         ChangeNotifierProvider.value(value: SettingsProvider.getInstance()),
         ChangeNotifierProvider(create: (_) => NotificationProvider()),
-        ChangeNotifierProvider.value(value: fduHoleProvider)
+        ChangeNotifierProvider.value(value: fduHoleProvider),
+        FutureProvider.value(value: GitInfo.get(), initialData: null)
       ], child: mainApp),
     );
   }
--- lib/page/subpage_settings.dart
+++ lib/page/subpage_settings.dart
@@ -56,6 +56,7 @@ import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_progress_dialog/flutter_progress_dialog.dart';
+import 'package:git_info/git_info.dart';
 import 'package:in_app_review/in_app_review.dart';
 import 'package:nil/nil.dart';
 import 'package:path_provider/path_provider.dart';
@@ -236,7 +237,9 @@ class SettingsSubpageState extends PlatformSubpageState<SettingsSubpage> {
     LicenseItem("device_identity", LICENSE_MIT,
         "https://devgit.starschina.com/flutter_open_srouce/device_identity"),
     LicenseItem("tutorial_coach_mark", LICENSE_MIT,
-        "https://github.com/RafaelBarbosatec/tutorial_coach_mark")
+        "https://github.com/RafaelBarbosatec/tutorial_coach_mark"),
+    LicenseItem("git_info", LICENSE_BSD_3_0_CLAUSE,
+        "https://github.com/Rexios80/git_info")
   ];
 
   String? _clearCacheSubtitle;
@@ -1033,10 +1036,15 @@ class SettingsSubpageState extends PlatformSubpageState<SettingsSubpage> {
                       //Version
                       Align(
                         alignment: Alignment.centerRight,
-                        child: Text(
-                          '${S.of(context).version} ${FlutterApp.versionName} build ${pubspec.build.first}',
-                          textScaleFactor: 0.7,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        child: Consumer<GitInformation?>(
+                          builder: (context, value, child) {
+                            return Text(
+                              '${S.of(context).version} ${FlutterApp.versionName} build ${pubspec.build.first} #${value?.hash?.substring(0, 7)}',
+                              textScaleFactor: 0.7,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            );
+                          },
                         ),
                       ),
                       const SizedBox(height: 4),
