d1bdaaa6abb060991e51cba4c0e75b2c77cefb1e
w568w <1278297578@qq.com>
2024-03-08 01:42:16 +0800

    fix: some incompatible APIs making it unable to run on web

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -346,6 +346,11 @@ class HomePageState extends State<HomePage> with WidgetsBindingObserver {
       }
     }
 
+    // fixme: uni_links *can* handle web, but our web version could have extra path
+    // by default (e.g. "DanXi/" in "https://danxi.fduhole.com/DanXi/"), which
+    // is recognized as an invalid path by [dealWithUri] now. Improve it later.
+    if (PlatformX.isWeb) return;
+
     Uri? initialUri;
     initialUri = await getInitialUri();
     if (initialUri != null) await dealWithUri(initialUri);
