9251d36b25520c7f137748721ddaa00a981c5e17
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2024-07-02 16:03:39 +0800

    Revert "fix: don't show banner if no banner elements"
    This reverts commit 064d6fdeff9c7c5c479ceaf7128ac8752f335943.

--- lib/widget/forum/auto_banner.dart
+++ lib/widget/forum/auto_banner.dart
@@ -158,11 +158,6 @@ class AutoBannerState extends State<AutoBanner> {
             return Container();
           }
 
-          // No need for banner if no banner elements
-          if (list!.isEmpty){
-            return Container();
-          }
-
           // Since the banner is not a fixed size, we need to use [SizedByChildBuilder]
           // to get the height of the banner. Otherwise, [Swiper] will have infinite
           // height bound and throw an exception during build.
