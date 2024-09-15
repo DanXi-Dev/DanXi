7a74c7809ab1033a3fd1263226f0fe1ca9aaf827
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2024-09-15 11:38:02 +0800

    change: make the course time more verbose Tweaked the styles of controls to adapt to longer text, avoid text overflow

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -36,10 +36,9 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) gtk_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "GtkPlugin");
   gtk_plugin_register_with_registrar(gtk_registrar);
-    g_autoptr(FlPluginRegistrar)
-    open_file_linux_registrar =
-            fl_plugin_registry_get_registrar_for_plugin(registry, "OpenFileLinuxPlugin");
-    open_file_linux_plugin_register_with_registrar(open_file_linux_registrar);
+  g_autoptr(FlPluginRegistrar) open_file_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "OpenFileLinuxPlugin");
+  open_file_linux_plugin_register_with_registrar(open_file_linux_registrar);
   g_autoptr(FlPluginRegistrar) platform_device_id_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "PlatformDeviceIdLinuxPlugin");
   platform_device_id_linux_plugin_register_with_registrar(platform_device_id_linux_registrar);
