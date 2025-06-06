06c14efa6a36689c0804ea967eaa0243d0812a0d
w568w <1278297578@qq.com>
2025-06-06 14:02:58 +0000

    Deploying to gh-pages from @ DanXi-Dev/DanXi@522e8caf8dec236784cdf116daffd5e40add1f4e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3405899680"
+    serviceWorkerVersion: "4072003664"
   }
 });
