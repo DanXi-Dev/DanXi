c9ff892abd6b27cf7b79b5f93373ed0c36b4b8b8
w568w <1278297578@qq.com>
2025-06-07 09:30:43 +0000

    Deploying to gh-pages from @ DanXi-Dev/DanXi@3d09b7146908a814fbd2673045f0c54405b47a28 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -90,11 +90,11 @@ const RESOURCES = {"canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "manifest.json": "012d91b9edf492757ddf24dd1b15d03e",
 "favicon.webp": "5d8a19c576a1ba2a24af3b99f340237d",
-"index.html": "548eaa34837c39fc8682cac5b24d718f",
-"/": "548eaa34837c39fc8682cac5b24d718f",
+"index.html": "7716b8c907e5e55af95c101a0f2e425c",
+"/": "7716b8c907e5e55af95c101a0f2e425c",
 "version.json": "e31afb07aabd4f4d840c38e41f998bc6",
-"flutter_bootstrap.js": "8c4dab052ef1b7f10cd712e31a7450d8",
-"main.dart.js": "92a6322fd93ebc7e78fd21aaedd9aa78"};
+"flutter_bootstrap.js": "6e19e158ba5087a8e9271df29b6b8972",
+"main.dart.js": "8115f565b883e3c63597abb9f40b91d8"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
