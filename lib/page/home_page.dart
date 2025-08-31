2f25e42e3b76365a2c63998978713dcc53a146d5
kooWZ <60533182+kooWZ@users.noreply.github.com>
2025-08-31 16:43:38 +0800

    fix: possible setState() called inside initState()

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -449,13 +449,16 @@ class HomePageState extends State<HomePage> with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
+
+    _loadPersonInfo();
+    _rebuildPage();
+
     // Refresh the page when account changes.
     StateProvider.isLoggedIn.addListener(() {
       _rebuildPage();
       _subpage[_pageIndex.value].onFirstShownAsHomepage(context);
       refreshSelf();
     });
-    _loadPersonInfo();
     initSystemTray().catchError((ignored) {});
     WidgetsBinding.instance.addObserver(this);
 
