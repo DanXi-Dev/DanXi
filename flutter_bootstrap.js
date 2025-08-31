c0dec086053f323ed86e3bc699d10c4aa1dc7ed6
w568w <1278297578@qq.com>
2025-08-31 13:44:41 +0000

    Deploying to gh-pages from @ DanXi-Dev/DanXi@618a1959f7f2175eee2765a943d8b2d580b5b50a 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f7f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a72","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3743408788"
+    serviceWorkerVersion: "86163774"
   }
 });
