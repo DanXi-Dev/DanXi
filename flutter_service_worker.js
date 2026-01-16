d796fa0bea5301938dfbe0f1d08333aa3617c5d6
w568w <1278297578@qq.com>
2026-01-16 05:39:17 +0000

    Deploying to gh-pages from @ DanXi-Dev/DanXi@c66f44853af20feb0047d0136fa9fce56482a691 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "manifest.json": "012d91b9edf492757ddf24dd1b15d03e",
-"index.html": "f51dc5b142b9df0363ad08961e840e8f",
-"/": "f51dc5b142b9df0363ad08961e840e8f",
+"index.html": "79780d47a06554ea34f6890cfc37f942",
+"/": "79780d47a06554ea34f6890cfc37f942",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "515e42c5e7776dea859a4ab4075ba4ad",
@@ -71,9 +71,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.webp": "5d8a19c576a1ba2a24af3b99f340237d",
-"flutter_bootstrap.js": "d10488e26ce912dad1c49bda67987995",
+"flutter_bootstrap.js": "234b14f94d1b21888834a64c606b161a",
 "version.json": "3ac35de81d647f650b6f3a674887919c",
-"main.dart.js": "172c62dcef9f441db09daa7ff51c492c"};
+"main.dart.js": "8193f900b2b3f0795579ac1a418ec8fc"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
