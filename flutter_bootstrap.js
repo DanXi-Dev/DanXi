eebf8184eb851f5b2cbb7f9eff24244920cac0a7
w568w <1278297578@qq.com>
2026-02-13 04:11:26 +0000

    Deploying to gh-pages from @ DanXi-Dev/DanXi@df1c8331cf750799c3e89e2987245de50f23b9fd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2067300081" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1780346317" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
