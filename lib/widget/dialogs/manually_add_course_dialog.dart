763a78e90eb8f84824345bdb324fdcf4c400580d
xeu <dan-xi@xeu.buhzzi.com>
2026-03-14 20:30:25 +0800

    refactor: concatenate dialogs for cohesion
    - Deleted `lib/widget/dialogs/manually_add_course_dialog_sub.dart`.
    - Appended the `AddCourseDialogSub` class and its state to
      `lib/widget/dialogs/manually_add_course_dialog.dart`.

--- lib/widget/dialogs/manually_add_course_dialog.dart
+++ lib/widget/dialogs/manually_add_course_dialog.dart
@@ -4,7 +4,6 @@ import 'package:dan_xi/generated/l10n.dart';
 import 'package:dan_xi/model/time_table.dart';
 import 'package:dan_xi/provider/settings_provider.dart';
 import 'package:dan_xi/util/platform_universal.dart';
-import 'package:dan_xi/widget/dialogs/manually_add_course_dialog_sub.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
@@ -288,5 +287,113 @@ class _ManuallyAddCourseDialogState extends State<ManuallyAddCourseDialog> {
 
   void _removeCourseTimeTile(int weekDay) {
     newCourse.times!.removeWhere((time) => time.weekDay == weekDay);
+  }
+}
+
+/// Slots selector for the course.
+class AddCourseDialogSub extends StatefulWidget {
+  const AddCourseDialogSub({super.key});
+
+  @override
+  State<AddCourseDialogSub> createState() => _AddCourseDialogSubState();
+}
+
+class _AddCourseDialogSubState extends State<AddCourseDialogSub> {
+  int selectedWeekDay = 0;
+  final selectedSlots = List.filled(
+    TimeTable.kCourseSlotStartTime.length,
+    false,
+  );
+  List<CourseTime>? newCourseTime;
+
+  List<CourseTime>? newCourseTimeGenerator(
+    int selectedWeekDay,
+    List<bool> selectedSlots,
+  ) {
+    return selectedSlots
+        .mapIndexed(
+          (index, selected) =>
+              selected ? CourseTime(selectedWeekDay, index) : null,
+        )
+        .nonNulls
+        .toList(growable: false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAlertDialog(
+      content: Column(
+        children: [
+          Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(7, (index) => index)
+                  .map((e) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedWeekDay = e;
+                          });
+                        },
+                        child: CircleAvatar(
+                          radius: 24.0,
+                          backgroundColor: Color(
+                              context.read<SettingsProvider>().primarySwatch),
+                          foregroundColor: Colors.white,
+                          child: e == selectedWeekDay
+                              ? Icon(PlatformX.isMaterial(context)
+                                  ? Icons.done
+                                  : CupertinoIcons.checkmark_alt)
+                              : Text(
+                                  Constant.weekDay(e),
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                        ),
+                      ))
+                  .toList()),
+          const SizedBox(height: 20),
+          Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(14, (index) => index)
+                  .map((e) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedSlots[e] = !selectedSlots[e];
+                          });
+                        },
+                        child: CircleAvatar(
+                          radius: 24.0,
+                          backgroundColor: Color(
+                              context.read<SettingsProvider>().primarySwatch),
+                          foregroundColor: Colors.white,
+                          child: selectedSlots[e] == true
+                              ? Icon(PlatformX.isMaterial(context)
+                                  ? Icons.done
+                                  : CupertinoIcons.checkmark_alt)
+                              : Text(
+                                  (e + 1).toString(),
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                        ),
+                      ))
+                  .toList()),
+        ],
+      ),
+      actions: [
+        PlatformDialogAction(
+            child: Text(S.of(context).cancel),
+            onPressed: () => Navigator.pop(context)),
+        PlatformDialogAction(
+            child: Text(S.of(context).add),
+            onPressed: () {
+              Navigator.pop(context,
+                  newCourseTimeGenerator(selectedWeekDay, selectedSlots));
+            }),
+      ],
+    );
   }
 }
